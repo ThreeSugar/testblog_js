d878c82f8d8273f55dba8cbc74f83bb5de6468a8
ThreeSugar <threesugar@hotmail.com>
2018-03-09 23:05:12 +0800

    finally done with login/logout

--- routes/logout.js
+++ routes/logout.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-	req.logout();
-	req.flash('logout', 'Successfully logged out!');
-	res.redirect('/login');
+	if(req.isAuthenticated()){
+		req.logout();
+		req.flash('logout', 'Successfully logged out!');
+		res.redirect('/login');
+	} else {
+		res.redirect('/login')
+	}
 });
 
 module.exports = router;
