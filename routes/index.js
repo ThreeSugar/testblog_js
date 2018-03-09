14a5e5f5c2fa1285cb232313b532d95b6101a3a9
ThreeSugar <threesugar@hotmail.com>
2018-03-09 20:24:48 +0800

    redirect to login page if not logged in

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  if(!req.user){
+    res.redirect('login');
+  } else {
+    res.render('index');
+  }
+
 });
 
 module.exports = router;
